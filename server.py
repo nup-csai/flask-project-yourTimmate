0368d7ffb0caf35e8d003021301c76f429cc152e
timmate <mr.timmate@gmail.com>
2024-11-12 15:10:57 +0200

    hi

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    with open('data.json', 'r') as file:
+    with open('database.json', 'r') as file:
         data = json.load(file)
     return f"{data}"
